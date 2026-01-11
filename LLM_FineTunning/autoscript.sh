0f380ac4e5e4d4ab0144836a21e41799a98a73cb
TomasM <tomas.bk148@gmail.com>
2026-01-11 21:28:07 +0100

    Docker compose file update and autocript update

--- LLM_FineTunning/autoscript.sh
+++ LLM_FineTunning/autoscript.sh
@@ -1,9 +1,8 @@
-cat > /tmp/Modelfile << 'EOF'
+cat > /tmp/Modelfile <<EOF
 FROM /root/.ollama/models/qwen2.5-1.5b-instruct-q4_k_m.gguf
 TEMPLATE "[INST] {{ .Prompt }} [/INST]"
 PARAMETER stop "[INST]"
 PARAMETER stop "[/INST]"
 EOF
 
-
-ollama create qwen2-model -f /tmp/Modelfile
+ollama create qwen2-model -f /tmp/Modelfile || true
