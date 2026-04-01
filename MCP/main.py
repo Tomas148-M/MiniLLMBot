ad817150479bfa9851da6dcebb9dbd8b458bf486
TomasM <tomas.bk148@gmail.com>
2026-04-01 21:33:55 +0200

    update

--- MCP/main.py
+++ MCP/main.py
@@ -8,13 +8,13 @@ try:
     from .tools.time_tool import register_time_tools
     from .tools.greeting_tools import register_greeting_tools
     from .tools.math_tools import register_math_tools
-    from .tools.text_tools import register_text_tools
+    from .tools.mcp_wheather import register_weather_tools
     
 except ImportError:
     # Works when run as script: python MCP/main.py
     from tools.greeting_tools import register_greeting_tools
     from tools.math_tools import register_math_tools
-    from tools.text_tools import register_text_tools
+    from tools.mcp_wheather import register_weather_tools
     from tools.time_tool import register_time_tools
 
 def create_main_server(name: str = "Main MCP Server") -> FastMCP:
@@ -24,14 +24,14 @@ def create_main_server(name: str = "Main MCP Server") -> FastMCP:
     register_math_tools(mcp)
     register_greeting_tools(mcp)
     register_time_tools(mcp)
-    register_text_tools(mcp)
+    register_weather_tools(mcp)
 
     return mcp
 
 
 def main() -> None:
     """Run the main server over SSE."""
-    create_main_server().run(transport="sse", port=8080)
+    create_main_server().run(transport="sse", host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
