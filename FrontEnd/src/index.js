8b3d7cb58af798c775ad535e5ce5111f295944ab
TomasM <tomas.bk148@gmail.com>
2026-04-11 21:58:29 +0200

    LLM3 file delete and everything moved to App.js file

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AIPromptChat from './LLM3';
+import AIPromptChat from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
